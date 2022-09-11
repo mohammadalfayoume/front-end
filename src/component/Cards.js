8706670347d862bf9d3cfcaab101971cfb442c5a
Lujain92 <lujainaljarrah92@gmail.com>
2022-09-11 12:30:23 +0300

    empty component

---
+++ src/component/Cards.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Modal from "./Modal";
+
+class Card extends React.Component{
+  render(){
+      return(
+          <>
+          Cards
+          <Modal/>
+          </>
+      )
+  }
+}
+
+export default Card
