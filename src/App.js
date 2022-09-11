f3f0c188b1baecad5e8d031aacd44377a0089fea
abeershboul <abeer.shnoul@yahoo.com>
2022-09-11 18:56:42 +0300

    header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from "react";
 import Header from "./component/Header";
 import Main from "./component/Main";
 import Footer from "./component/Footer";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 class App extends React.Component{
   render(){
       return(
           <>
-          <Header/>
+          <Router>
+          <Header />
+          <Routes>
+            
+         
+            
+              <Route exact path="/" element={<Main />}></Route>
+              {/* <Route exact path="/profile" element={<About />}></Route>
+              <Route exact path="/contact" element={<Contact />}></Route>
+            
+     */}
+
+           
+          </Routes>
+          
+        </Router>
           <Main/>
           <Footer/>
           </>
